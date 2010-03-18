43f6dc04fbaade75d51aecaba598b2a2302fb0a6
Evgeny Mandrikov <mandrikov@gmail.com>
2010-03-18 19:21:07 +0000

    JIRA: - Add more tests - Update to Parent POM version 5

---
+++ src/test/java/org/sonar/plugins/jira/JiraSensorTest.java
@@ -0,0 +1,50 @@
+/*
+ * Sonar, open source software quality management tool.
+ * Copyright (C) 2009 SonarSource SA
+ * mailto:contact AT sonarsource DOT com
+ *
+ * Sonar is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * Sonar is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Sonar; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+package org.sonar.plugins.jira;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.sonar.api.resources.Project;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * @author Evgeny Mandrikov
+ */
+public class JiraSensorTest {
+  private JiraSensor sensor;
+
+  @Before
+  public void setUp() {
+    sensor = new JiraSensor();
+  }
+
+  @Test
+  public void testShouldExecuteOnProject() throws Exception {
+    Project project = mock(Project.class);
+    when(project.isRoot()).thenReturn(true).thenReturn(false);
+    
+    assertThat(sensor.shouldExecuteOnProject(project), is(true));
+    assertThat(sensor.shouldExecuteOnProject(project), is(false));
+  }
+}
